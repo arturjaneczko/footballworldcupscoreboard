e80c5a0b40d96c8bb6455469652e53de1088714d
arturjaneczko <janeczko.artur@gmail.com>
2026-03-04 17:11:45 +0100

    start game test

--- src/test/java/com/sportradar/fwcsb/domain/BoardTest.java
+++ src/test/java/com/sportradar/fwcsb/domain/BoardTest.java
@@ -1,11 +1,12 @@
 package com.sportradar.fwcsb.domain;
 
+1import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BoardTest {
 
     @Test
     void testStartGame() {
-        new Board();
+        Assertions.assertThat(new Board().startGame()).isTrue();
     }
 }
